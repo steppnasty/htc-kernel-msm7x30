zOrg1331/kernel-image-cuboxi

ARM: Convert platform reservations to use LMB rather than bootmem
Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

arch/arm/kernel/setup.c
@@ -714,7 +714,7 @@ void __init setup_arch(char **cmdline_p)
 
 	parse_early_param();
 
-	arm_lmb_init(&meminfo);
+	arm_lmb_init(&meminfo, mdesc);
 
 	paging_init(mdesc);
 	request_standard_resources(&meminfo, mdesc);

arch/arm/mach-clps711x/edb7211-arch.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <linux/init.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/types.h>
 #include <linux/string.h>
 
@@ -33,7 +33,7 @@ extern void edb7211_map_io(void);
 /* Reserve screen memory region at the start of main system memory. */
 static void __init edb7211_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, 0x00020000, BOOTMEM_DEFAULT);
+	lmb_reserve(PHYS_OFFSET, 0x00020000);
 }
 
 static void __init

arch/arm/mach-integrator/core.c
@@ -14,7 +14,7 @@
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/sched.h>
 #include <linux/smp.h>
 #include <linux/termios.h>
@@ -225,5 +225,5 @@ EXPORT_SYMBOL(cm_control);
  */
 void __init integrator_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET, BOOTMEM_DEFAULT);
+	lmb_reserve(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET);
 }

arch/arm/mach-pxa/palmt5.c
@@ -21,7 +21,7 @@
 #include <linux/irq.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/pda_power.h>
 #include <linux/pwm_backlight.h>
 #include <linux/gpio.h>
@@ -399,7 +399,7 @@ static void __init palmt5_udc_init(void)
 
 static void __init palmt5_reserve(void)
 {
-	reserve_bootmem(0xa0200000, 0x1000, BOOTMEM_EXCLUSIVE);
+	lmb_reserve(0xa0200000, 0x1000);
 }
 
 static void __init palmt5_init(void)

arch/arm/mach-pxa/palmtreo.c
@@ -20,7 +20,7 @@
 #include <linux/irq.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/pda_power.h>
 #include <linux/pwm_backlight.h>
 #include <linux/gpio.h>
@@ -636,8 +636,8 @@ static void __init treo_lcd_power_init(void)
 
 static void __init treo_reserve(void)
 {
-	reserve_bootmem(0xa0000000, 0x1000, BOOTMEM_EXCLUSIVE);
-	reserve_bootmem(0xa2000000, 0x1000, BOOTMEM_EXCLUSIVE);
+	lmb_reserve(0xa0000000, 0x1000);
+	lmb_reserve(0xa2000000, 0x1000);
 }
 
 static void __init treo_init(void)

arch/arm/mach-s3c2440/mach-rx3715.c
@@ -15,7 +15,7 @@
 #include <linux/types.h>
 #include <linux/interrupt.h>
 #include <linux/list.h>
-#include <linux/bootmem.h>
+#include <linux/lmb.h>
 #include <linux/timer.h>
 #include <linux/init.h>
 #include <linux/tty.h>
@@ -195,8 +195,8 @@ static void __init rx3715_map_io(void)
 /* H1940 and RX3715 need to reserve this for suspend */
 static void __init rx3715_reserve(void)
 {
-	reserve_bootmem(0x30003000, 0x1000, BOOTMEM_DEFAULT);
-	reserve_bootmem(0x30081000, 0x1000, BOOTMEM_DEFAULT);
+	lmb_reserve(0x30003000, 0x1000);
+	lmb_reserve(0x30081000, 0x1000);
 }
 
 static void __init rx3715_init_irq(void)

arch/arm/mm/init.c
@@ -271,7 +271,7 @@ static void arm_memory_present(struct meminfo *mi)
 }
 #endif
 
-void __init arm_lmb_init(struct meminfo *mi)
+void __init arm_lmb_init(struct meminfo *mi, struct machine_desc *mdesc)
 {
 	int i;
 
@@ -296,9 +296,13 @@ void __init arm_lmb_init(struct meminfo *mi)
 #endif
 
 	arm_mm_lmb_reserve();
+
+	/* reserve any platform specific lmb areas */
+	if (mdesc->reserve)
+		mdesc->reserve();
 }
 
-void __init bootmem_init(struct machine_desc *mdesc)
+void __init bootmem_init(void)
 {
 	struct meminfo *mi = &meminfo;
 	unsigned long min, max_low, max_high;
@@ -308,9 +312,6 @@ void __init bootmem_init(struct machine_desc *mdesc)
 	find_limits(mi, &min, &max_low, &max_high);
 
 	arm_bootmem_init(mi, min, max_low);
-
-	if (mdesc->reserve)
-		mdesc->reserve();
 
 	/*
 	 * Sparsemem tries to allocate bootmem in memory_present(),

steppnasty/htc-kernel-msm7x30

arch/arm/kernel/setup.c
@@ -855,7 +855,7 @@ void __init setup_arch(char **cmdline_p)
 
 	parse_early_param();
 
-	arm_memblock_init(&meminfo);
+	arm_memblock_init(&meminfo, mdesc);
 
 	paging_init(mdesc);
 	request_standard_resources(&meminfo, mdesc);

arch/arm/mach-clps711x/edb7211-arch.c
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <linux/init.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/types.h>
 #include <linux/string.h>
 
@@ -33,7 +33,7 @@ extern void edb7211_map_io(void);
 /* Reserve screen memory region at the start of main system memory. */
 static void __init edb7211_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, 0x00020000, BOOTMEM_DEFAULT);
+	memblock_reserve(PHYS_OFFSET, 0x00020000);
 }
 
 static void __init

arch/arm/mach-integrator/core.c
@@ -14,7 +14,7 @@
 #include <linux/spinlock.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/sched.h>
 #include <linux/smp.h>
 #include <linux/termios.h>
@@ -230,5 +230,5 @@ EXPORT_SYMBOL(cm_control);
  */
 void __init integrator_reserve(void)
 {
-	reserve_bootmem(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET, BOOTMEM_DEFAULT);
+	memblock_reserve(PHYS_OFFSET, __pa(swapper_pg_dir) - PHYS_OFFSET);
 }

arch/arm/mach-pxa/palmt5.c
@@ -21,7 +21,7 @@
 #include <linux/irq.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/pda_power.h>
 #include <linux/pwm_backlight.h>
 #include <linux/gpio.h>
@@ -399,7 +399,7 @@ static void __init palmt5_udc_init(void)
 
 static void __init palmt5_reserve(void)
 {
-	reserve_bootmem(0xa0200000, 0x1000, BOOTMEM_EXCLUSIVE);
+	memblock_reserve(0xa0200000, 0x1000);
 }
 
 static void __init palmt5_init(void)

arch/arm/mach-pxa/palmtreo.c
@@ -20,7 +20,7 @@
 #include <linux/irq.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/pda_power.h>
 #include <linux/pwm_backlight.h>
 #include <linux/gpio.h>
@@ -636,8 +636,8 @@ static void __init treo_lcd_power_init(void)
 
 static void __init treo_reserve(void)
 {
-	reserve_bootmem(0xa0000000, 0x1000, BOOTMEM_EXCLUSIVE);
-	reserve_bootmem(0xa2000000, 0x1000, BOOTMEM_EXCLUSIVE);
+	memblock_reserve(0xa0000000, 0x1000);
+	memblock_reserve(0xa2000000, 0x1000);
 }
 
 static void __init treo_init(void)

arch/arm/mach-s3c2440/mach-rx3715.c
@@ -15,7 +15,7 @@
 #include <linux/types.h>
 #include <linux/interrupt.h>
 #include <linux/list.h>
-#include <linux/bootmem.h>
+#include <linux/memblock.h>
 #include <linux/timer.h>
 #include <linux/init.h>
 #include <linux/tty.h>
@@ -195,8 +195,8 @@ static void __init rx3715_map_io(void)
 /* H1940 and RX3715 need to reserve this for suspend */
 static void __init rx3715_reserve(void)
 {
-	reserve_bootmem(0x30003000, 0x1000, BOOTMEM_DEFAULT);
-	reserve_bootmem(0x30081000, 0x1000, BOOTMEM_DEFAULT);
+	memblock_reserve(0x30003000, 0x1000);
+	memblock_reserve(0x30081000, 0x1000);
 }
 
 static void __init rx3715_init_irq(void)

arch/arm/mm/init.c
@@ -271,7 +271,7 @@ static void arm_memory_present(struct meminfo *mi)
 }
 #endif
 
-void __init arm_memblock_init(struct meminfo *mi)
+void __init arm_memblock_init(struct meminfo *mi, struct machine_desc *mdesc)
 {
 	int i;
 
@@ -297,11 +297,15 @@ void __init arm_memblock_init(struct meminfo *mi)
 
 	arm_mm_memblock_reserve();
 
+	/* reserve any platform specific memblock areas */
+	if (mdesc->reserve)
+		mdesc->reserve();
+
 	memblock_analyze();
 	memblock_dump_all();
 }
 
-void __init bootmem_init(struct machine_desc *mdesc)
+void __init bootmem_init(void)
 {
 	struct meminfo *mi = &meminfo;
 	unsigned long min, max_low, max_high;
@@ -311,9 +315,6 @@ void __init bootmem_init(struct machine_desc *mdesc)
 	find_limits(mi, &min, &max_low, &max_high);
 
 	arm_bootmem_init(mi, min, max_low);
-
-	if (mdesc->reserve)
-		mdesc->reserve();
 
 	/*
 	 * Sparsemem tries to allocate bootmem in memory_present(),